4af411b5e1ce3b891e9e26a888fcd48bd6f0121e
kim <kimemmanuel004@gmail.com>
2022-09-10 16:51:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+    });
+
+//what if we wanted to use that same alert message on a bunch of elements?    
+/*
+    const input = document.getElementById('input');
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    input.addEventListener('click', clickAlert);
+*/    
 }
